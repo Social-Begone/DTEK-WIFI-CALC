3474efbf8280b850d906df43b8b434b33c40c8e3
henrykkebaby <59703378+henrykkebaby@users.noreply.github.com>
2020-02-29 12:18:37 +0100

    weebisgay

--- storystring.c
+++ storystring.c
@@ -1,4 +1,4 @@
-#include "sample.c"
+#include "sample.h"
 #include <stdint.h>
 
 /*
@@ -19,291 +19,291 @@ fourlines(
 //Start
 
 fourlines(
-"Oh, Hello!"
-"Are you starting"
-"this math course"
+"Oh, Hello!",
+"Are you starting",
+"this math course",
 "as well?"
 );
 
 
 
 threelines_and_menu(
-"Yeah, I guess"
-"we'll be stuck"
+"Yeah, I guess",
+"we'll be stuck",
 "together."
 );
 fourlines(
-"I wouldn't mind"
-"being stuck with"
-"you if you are"
+"I wouldn't mind",
+"being stuck with",
+"you if you are",
 "good with math."
 );
 
 
 threelines_and_menu(
-"I am! Wanna grab"
-"a seat in the"
+"I am! Wanna grab",
+"a seat in the",
 "classroom?"
 );
 fourlines(
-"Yeah lets sit"
-"together!"
-"Are you any good"
+"Yeah lets sit",
+"together!",
+"Are you any good",
 "with math?"
 );
 
 
 threelines_and_menu(
-"Hi, yeah I will"
-"be reading this"
+"Hi, yeah I will",
+"be reading this",
 "course."
 );
 fourlines(
-"Right, tell me."
-"Are you any good"
-"with math?"
+"Right, tell me.",
+"Are you any good",
+"with math?",
 ""
 );
 
 //next response
 
 threelines_and_menu(
-"Well, I am kinda"
-"getting the hang"
+"Well, I am kinda",
+"getting the hang",
 "of it."
 );
 fourlines(
-"Hmmm, maybe we"
-"can study after"
-"class? Two is"
+"Hmmm, maybe we",
+"can study after",
+"class? Two is",
 "better than one."
 );
 
 
 threelines_and_menu(
-"I mean, I am ok"
-"at it i guess."
+"I mean, I am ok",
+"at it i guess.",
 ""
 );
 fourlines(
-"I feel behind on"
-"math in general"
-"maybe we can do"
+"I feel behind on",
+"math in general",
+"maybe we can do",
 "a study group?"
 );
 
 
 threelines_and_menu(
-"Maybe with you"
-"around I'll get"
-"the hang of it."
+"Maybe with you",
+"around I'll get",
+"the hang of it.",
 );
 fourlines(
-"I feel the same."
-"We should totaly"
-"study together"
+"I feel the same.",
+"We should totaly",
+"study together",
 "after class!"
 );
 
 //next response
 
 threelines_and_menu(
-"Maybe we'll"
-"start one"
+"Maybe we'll",
+"start one",
 "someday."
 );
 fourlines(
-"Right, maybe."
-"Let's head into"
-"class."
+"Right, maybe.",
+"Let's head into",
+"class.",
 ""
 );
 
 threelines_and_menu(
-"I won't survive"
-"this course any"
+"I won't survive",
+"this course any",
 "other way."
 );
 fourlines(
-"Come on! Let's"
-"sit together in"
-"class!"
+"Come on! Let's",
+"sit together in",
+"class!",
 ""
 );
 
 threelines_and_menu(
-"Together we will"
-"ace the course"
+"Together we will",
+"ace the course",
 "for sure!"
 );
 fourlines(
-"For sure! Come"
-"our lesson is"
-"about to start!"
+"For sure! Come",
+"our lesson is",
+"about to start!",
 ""
 );
 
 //monologue1
 
 fourlines(
-"\"Meet me at the"
-"cafe once you're"
-"finished today.\""
+"\"Meet me at the",
+"cafe once you're",
+"finished today.\"",
 ""
 );
 fourlines(
-"The day flies by"
-"as you attend"
-"classes filled"
+"The day flies by",
+"as you attend",
+"classes filled",
 "with knowledge."
 );
 fourlines(
-"As you finish "
-"your last lesson"
-"for the day you"
+"As you finish",
+"your last lesson",
+"for the day you",
 "remember..."
 );
 fourlines(
-"You are late to"
-"the cafe! So"
-"you hurry out"
+"You are late to",
+"the cafe! So",
+"you hurry out",
 "the building..."
 );
 
 //Minigame
 
 fourlines(
-"Hi, you!"
-"Ready to get"
-"started?"
+"Hi, you!",
+"Ready to get",
+"started?",
 ""
 );
 
 
 
 threelines_and_menu(
-"Let's study like"
-"never before!"
+"Let's study like",
+"never before!",
 ""
 );
 fourlines(
-"Hell yeah! This"
-"is going to be a"
-"great studying"
+"Hell yeah! This",
+"is going to be a",
+"great studying",
 "session!"
 );
 
 
 threelines_and_menu(
-"I am ready now"
-"that I am with"
+"I am ready now",
+"that I am with",
 "you."
 );
 fourlines(
-"Mmmmmmm"
-"I'd always study"
-"if I got to meet"
+"Mmmmmmm",
+"I'd always study",
+"if I got to meet",
 "you."
 );
 
 
 threelines_and_menu(
-"Let's just dish"
-"this days class"
+"Let's just dish",
+"this days class",
 "off"
 );
 fourlines(
-"You're right."
-"let's get on"
-"with the work."
+"You're right.",
+"let's get on",
+"with the work.",
 ""
 );
 
 //monologue2
 
 fourlines(
-"Both of you sit"
-"and run through"
-"the papers."
+"Both of you sit",
+"and run through",
+"the papers.",
 ""
 );
 fourlines(
-"Answering the"
-"questions while"
-"the cafe buzz"
+"Answering the",
+"questions while",
+"the cafe buzz",
 "around you two."
 );
 fourlines(
-"You sit and work"
-"in full focus"
-"when you realize"
+"You sit and work",
+"in full focus",
+"when you realize",
 "the time..."
 );
 
 //Dialogue
 
 fourlines(
-"It is getting"
-"late, what are"
-"you doing later"
+"It is getting",
+"late, what are",
+"you doing later",
 "this evening?"
 );
 
 
 
 threelines_and_menu(
-"If you are free"
-"we could hang"
+"If you are free",
+"we could hang",
 "some more."
 );
 fourlines(
-"I wouldn't mind"
-"spending more"
-"time with you."
+"I wouldn't mind",
+"spending more",
+"time with you.",
 "what do you say?"
 );
 
 threelines_and_menu(
-"Nothing planned"
-"I'd love to do"
+"Nothing planned",
+"I'd love to do",
 "something though"
 );
 fourlines(
-"Maybe we could"
-"figure something"
-"out, what do you"
+"Maybe we could",
+"figure something",
+"out, what do you",
 "say?"
 );
 
 threelines_and_menu(
-"I don't know, I"
-"might go home,"
+"I don't know, I",
+"might go home,",
 "watch a movie."
 );
 fourlines(
-"Don't you want"
-"to do something"
-"else later"
+"Don't you want",
+"to do something",
+"else later",
 "this evening?"
 );
 
 //Last Dialogue
 
 threelines_and_menu(
-"We should def."
-"do something"
+"We should def.",
+"do something",
 "tonight!"
 );
 
 threelines_and_menu(
-"Nah maybe"
-"not sure."
+"Nah maybe",
+"not sure.",
 ""
 );
 
 threelines_and_menu(
-"I'd love to!"
-""
+"I'd love to!",
+"",
 ""
 );
 
