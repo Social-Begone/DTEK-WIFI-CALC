fc2d7860866e1862c22323a4b4f744553a9bee99
henrykkebaby <henrykkebaby@users.noreply.github.com>
2020-02-29 19:14:37 +0100

    headers

--- data.h
+++ data.h
@@ -3,6 +3,9 @@
 // images
 extern const uint8_t const image_menu[128*4];
 extern const uint8_t const image_test[128*4];
+extern const uint8_t const image_intro[128*4];
+extern const uint8_t const image_outro[128*4];
+extern const uint8_t const image_dialogue_controls[128*4];
 extern const uint64_t const ext_screen_buffer[2*64];
 
 #endif
