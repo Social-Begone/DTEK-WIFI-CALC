88ddf9f1a6ffb3f311e11498fff4450935ebc1d3
Tobiky <andreas.hammarstrand@gmail.com>
2020-03-01 17:39:34 +0100

    added source pictures

--- data.h
+++ data.h
@@ -10,6 +10,8 @@ extern const uint8_t const image_dialogue_controls[128*4];
 extern const uint8_t const image_minigame_controls[128*4];
 
 
-extern const uint64_t const ext_screen_buffer[2*64];
+extern const uint64_t const image_anime_neutral[2*64];
+extern const uint64_t const image_anime_loving[2*64];
+extern const uint64_t const image_anime_friendly[2*64];
 
 #endif
